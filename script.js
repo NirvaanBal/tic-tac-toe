6c27d75b77120e2095eff9152db4ea6d744c0c29
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-18 09:14:38 +0530

    Create factory for players and modules for gameboard and game

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('working...');
+const Gameboard = (() => {
+  let gameBoard = ['', '', '', '', '', '', '', '', ''];
+
+  return { gameBoard };
+})();
+
+const Players = () => {
+  let p1, p2;
+
+  return { p1, p2 };
+};
+
+const Game = (() => {})();
