1cf578fbc877169a83f6de4c1c23d2ae320e63a6
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-19 09:14:29 +0530

    Add game logic to check for winner

--- script.js
+++ script.js
@@ -21,34 +21,91 @@ const Gameboard = (() => {
 
 const Player = () => {
   const _markers = ['O', 'X'];
-  const playerStartingMarker = _markers[Math.floor(Math.random() * 2)];
+  const _playerStartingMarker = _markers[Math.floor(Math.random() * 2)];
+  let player = _playerStartingMarker;
 
-  return { marker: playerStartingMarker };
+  return { player };
 };
 
 const Game = (() => {
-  let _playerMarker = Player().marker;
+  let _player = Player().player;
+  let winner;
 
   const _toggleMarker = (marker) => {
-    return marker === 'O' ? (_playerMarker = 'X') : (_playerMarker = 'O');
+    return marker === 'O' ? (_player = 'X') : (_player = 'O');
   };
 
-  const _boxSelected = () => {
+  const action = () => {
     document.querySelectorAll('.box').forEach((box, index) => {
       box.addEventListener('click', (e) => {
         if (e.target.dataset.disable === 'false') {
-          Gameboard.gameBoard[index] = _playerMarker;
-          e.target.textContent = _playerMarker;
+          Gameboard.gameBoard[index] = _player;
+          e.target.textContent = _player;
           e.target.dataset.disable = 'true';
-          _toggleMarker(_playerMarker);
+          result(Gameboard.gameBoard, _player);
+          _toggleMarker(_player);
         }
       });
     });
   };
 
+  const result = (gameArray, value) => {
+    if (
+      gameArray[0] === value &&
+      gameArray[1] === value &&
+      gameArray[2] === value
+    ) {
+      winner = _player;
+    } else if (
+      gameArray[3] === value &&
+      gameArray[4] === value &&
+      gameArray[5] === value
+    ) {
+      winner = _player;
+    } else if (
+      gameArray[6] === value &&
+      gameArray[7] === value &&
+      gameArray[8] === value
+    ) {
+      winner = _player;
+    } else if (
+      gameArray[0] === value &&
+      gameArray[3] === value &&
+      gameArray[6] === value
+    ) {
+      winner = _player;
+    } else if (
+      gameArray[1] === value &&
+      gameArray[4] === value &&
+      gameArray[7] === value
+    ) {
+      winner = _player;
+    } else if (
+      gameArray[2] === value &&
+      gameArray[5] === value &&
+      gameArray[8] === value
+    ) {
+      winner = _player;
+    } else if (
+      gameArray[0] === value &&
+      gameArray[4] === value &&
+      gameArray[8] === value
+    ) {
+      winner = _player;
+    } else if (
+      gameArray[2] === value &&
+      gameArray[4] === value &&
+      gameArray[6] === value
+    ) {
+      winner = _player;
+    }
+
+    if (winner) console.log(winner);
+  };
+
   const init = () => {
     Gameboard.draw();
-    _boxSelected();
+    action();
   };
 
   return { init };
