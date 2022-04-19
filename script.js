8cdb4dd1c83de224ad5a940bae57369acbfaa8ca
Sukhdeep Singh <sukh.butalia@gmail.com>
2022-04-19 13:27:02 +0530

    Add code for two players to enter name and add logic for winner to get first turn in the next game

--- script.js
+++ script.js
@@ -26,7 +26,11 @@ const Player = () => {
   const _playerStartingMarker = _markers[Math.floor(Math.random() * 2)];
   let player = _playerStartingMarker;
 
-  return { player };
+  const playerName = (id) => {
+    return document.getElementById(id);
+  };
+
+  return { player, playerName };
 };
 
 const Game = (() => {
@@ -34,6 +38,7 @@ const Game = (() => {
   let _winner,
     _counter = 0;
   let _winnerEl = document.querySelector('.winner');
+  let _firstEl = document.querySelector('.first');
   const _resetBtn = document.querySelector('.reset');
 
   const _toggleMarker = (marker) => {
@@ -94,9 +99,14 @@ const Game = (() => {
 
   const _reset = () => {
     Gameboard.gameBoard = Gameboard.gameBoard.map((_) => '');
-    _player = Player().player;
+    _player = _winner || Player().player;
+    if (!_winner) {
+      Player().playerName('playerO').value = 'O';
+      Player().playerName('playerX').value = 'X';
+    }
     _winner = undefined;
     _counter = 0;
+
     Game.init();
   };
 
@@ -106,7 +116,7 @@ const Game = (() => {
   });
 
   const action = () => {
-    document.querySelector('.first').textContent = `Player ${_player}`;
+    _firstEl.textContent = `"${_player}" will start`;
     document.querySelectorAll('.box').forEach((box, index) => {
       box.addEventListener('click', (e) => {
         _counter++;
@@ -120,7 +130,10 @@ const Game = (() => {
         }
 
         if (_winner) {
-          _winnerEl.textContent = `Player "${_winner}" won`;
+          const _player1 = Player().playerName('playerO').value;
+          const _player2 = Player().playerName('playerX').value;
+          let champion = _winner === 'O' ? _player1 : _player2;
+          _winnerEl.textContent = `${champion} won`;
           _reset();
         } else if (!_winner && _counter === 9) {
           _winnerEl.textContent = `It's a tie!`;
